79435a6616c6e397f23d791637cc3e1495836fd2
gildavilleda <36199833+gildavilleda@users.noreply.github.com>
2021-04-01 22:45:02 -0600

    Add files via upload

---
+++ functions/Config.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: gguerrero
+ * Date: 28/02/2019
+ * Time: 03:08 PM
+ */
+
+define("DBHOST", "172.16.0.153");       //Establece la IP del Servidor 181.114.24.118
+define("DBNAME", "HNE001");             //Nombre de La base de datos
+define("CHARSET", "charset=UTF-8");     //Charset que se utilizara
+//define("DRIVER", "dblib:version=8.0");  //Libreria a utilizar
+define("DRIVER", "sqlsrv");
+
